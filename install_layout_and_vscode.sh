d9bae8cde61fc31aa6a5f30b595f007c6c314cf5
takenX10 <frau.alessandro01@gmail.com>
2023-01-17 14:29:14 +0100

    fixed autoinstaller

--- install_layout_and_vscode.sh
+++ install_layout_and_vscode.sh
@@ -1,12 +1,19 @@
-curl https://github.com/takenX10/takenX10-layouts/releases/tag/0.1.0/layout-build-v0.1.tar
-tar -xvf layout-build-v0.1.tar
-cd autoinstall
 
-curl https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/code_1.74.3-1673284829_amd64.deb
-sudo dpkg -i code_1.74.3-1673284829_amd64.deb
+echo """
+wget https://github.com/takenX10/takenX10-layouts/releases/download/0.1.0/layout-build-v0.1.tar
+tar -xzvf layout-build-v0.1.tar
+xkbcomp -w 0 autoinstall/xkbmap.xkb $DISPLAY
 
-cp keybindings.json ~/.config/Code/User/keybindings.json
-cp settings.json ~/.config/Code/User/settings.json
-cp -rf extensions .vscode/.
+cp autoinstall/keybindings.json data/user-data/keybindings.json
+cp autoinstall/settings.json data/user-data/settings.json
+cp -rf autoinstall/extensions data/extensions
 
-xkbcomp -w 0 xkbmap.xkb $DISPLAY
+""" > part2.sh
+
+chmod +x part2.sh
+
+wget https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/code-stable-x64-1673285154.tar.gz
+tar -xzvf code-stable-x64-1673285154.tar.gz
+cd VSCode-linux-x64
+mkdir data
+./code
