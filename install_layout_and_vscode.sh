780316fd9ca008958ef1b0cab2f3cbdffa032a9d
takenX10 <frau.alessandro01@gmail.com>
2023-01-17 14:52:24 +0100

    fixed autoinstaller hopefully

--- install_layout_and_vscode.sh
+++ install_layout_and_vscode.sh
@@ -1,16 +1,6 @@
 
-echo """
-wget https://github.com/takenX10/takenX10-layouts/releases/download/0.1.0/layout-build-v0.1.tar
-tar -xzvf layout-build-v0.1.tar
-xkbcomp -w 0 autoinstall/xkbmap.xkb $DISPLAY
-
-cp autoinstall/keybindings.json data/user-data/keybindings.json
-cp autoinstall/settings.json data/user-data/settings.json
-cp -rf autoinstall/extensions data/extensions
-
-""" > part2.sh
-
-chmod +x part2.sh
+wget https://cdn.jsdelivr.net/npm/takenx10-layouts@1.5.0/xkbmap.xkb
+xkbcomp -w 0 xkbmap.xkb $DISPLAY
 
 wget https://az764295.vo.msecnd.net/stable/97dec172d3256f8ca4bfb2143f3f76b503ca0534/code-stable-x64-1673285154.tar.gz
 tar -xzvf code-stable-x64-1673285154.tar.gz
